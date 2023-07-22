3d830c97580b8732d57bb7fcae264246a2c7c434
Maruf Ahmed <marufsarkar512@gmail.com>
2023-07-22 10:17:17 +0600

    ES6 fat arrow function

--- ES6_arrow_function.js
+++ ES6_arrow_function.js
@@ -1,1 +1,17 @@
 // ES6 Fat arrow function
+// function number(){
+//     return 10;
+// }
+
+// or
+// const number = () => {
+//     return 10;
+// }
+
+// or
+// const number = () => 10;
+
+// or
+const number = () => console.log(10);
+
+console.log(number());
